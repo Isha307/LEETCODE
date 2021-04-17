898c9e7d7b9b22734112d126d3dac58406ce9089
Isha307 <56998032+Isha307@users.noreply.github.com>
2021-04-17 20:24:55 +0530

    Create Restaurant Customers.cpp

---
+++ Codes_solutions/CSES/Restaurant Customers.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main()
+{
+  //ios_base::sync_with_stdio(false);
+  //cin.tie(NULL);
+  int n, x, y, maxi = 0, curr = 0;
+  vector<pair<int, bool>> arr;
+  cin >> n;
+  while (n--)
+  {
+    cin >> x >> y;
+    arr.push_back(make_pair(x, true));
+    arr.push_back(make_pair(y, false));
+    //cout<<arr<<endl;
+  }
+  sort(arr.begin(), arr.end());
+
+  for (int i = 0; i < arr.size(); i++)
+  {
+   // cout<<arr[i].first<<" "<<arr[i].second<<endl;
+    if(arr[i].second) curr++;
+    else curr--;
+    //cout<<curr<<endl;
+    maxi = max(maxi, curr);
+  }
+  cout << maxi;
+  return 0;
+}
