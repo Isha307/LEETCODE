582b5c3ac2443ec8f4ff8d621dd6d251c9b3a588
Isha307 <56998032+Isha307@users.noreply.github.com>
2021-05-28 17:47:35 +0530

    Create Remove Element.cpp

---
+++ LeetCode/Remove Element.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+         int n = nums.size(), count=0;
+        for(int i=0;i<n;i++)
+        {
+         if(nums[i]!=val) {nums[count++]= nums[i];
+                          }
+        }
+        return count;
+    }
+};
