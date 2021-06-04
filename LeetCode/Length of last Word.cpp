392f98c1535bdceb8809626d53a9085398a214a6
Isha307 <56998032+Isha307@users.noreply.github.com>
2021-06-04 10:09:37 +0530

    Create Length of last Word.cpp

---
+++ LeetCode/Length of last Word.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n = s.size()-1,len=0;
+        while(n>=0 && s[n]==' ') n--;
+         while(n>=0 && s[n]!=' ')
+         {
+            n--;
+           len++;          
+         }
+        return len;
+    }
+};
